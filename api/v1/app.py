88f6266a47f7b23f64ed15c6b2679f4baf7bb35f
elameen37 <elameen37@gmail.com>
2023-05-20 01:02:44 +0100

    update file

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 """swagger = swagger(app)"""
 db = os.environ.get('HBNB_TYPE_STORAGE', 'json_file')
 host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
-port = os.environ.get('HBNB_API_PORT', '5000')
+port = os.environ.get('HBNB_API_PORT', '5002')
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
